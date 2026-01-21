fee226310bb65aa225d0ea98630f25411718f75b
PolRibas <polribasrovira@gmail.com>
2026-01-21 09:40:57 +0100

    delete TailwindCss

--- apps/client/web/src/shims-tailwind.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module 'tailwindcss' {
-  // Minimal shim for editor/TS-server when tailwind types aren't resolved
-  export type Config = any;
-  const _default: any;
-  export default _default;
-}
--- packages/ui/tailwindConfig/src/shims-tailwind.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module 'tailwindcss' {
-  export type Config = any;
-  const _default: any;
-  export default _default;
-}
